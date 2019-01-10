2718f97117423f78ad003796877185374f35068d
leah <33074399+leahteich@users.noreply.github.com>
2019-01-10 15:20:45 -0500

    d

---
+++ leahcode/RandomGenerate.java
@@ -0,0 +1,15 @@
+public class RandomGenerate {
+
+public static int[] randNum() {
+    int max = 12;
+    int min = 0;
+    int range = max - min + 1;
+
+    for (int i = 0; i < 12; i++) {
+       int rand = (int)(Math.random() * range) + min;
+       System.out.println(rand);
+   }
+ }
+
+
+}
