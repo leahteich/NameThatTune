3b112ccff376bb1d060aef76f5fcb175de927304
leah <33074399+leahteich@users.noreply.github.com>
2019-01-13 13:50:38 -0500

    dw

--- leahcode/RandomGenerate.java
+++ leahcode/RandomGenerate.java
@@ -5,10 +5,9 @@ public static int[] randNum() {
     int min = 0;
     int range = max - min + 1;
 
-    for (int i = 0; i < 10; i++) {
-       int rand = (int)(Math.random() * range) + min;
-       System.out.println(rand);
-   }
+    int rand = (int)(Math.random() * range) + min;
+    System.out.println(rand);
+
  }
 
 
