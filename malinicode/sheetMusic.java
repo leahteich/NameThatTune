bce6105e95591c4e25057ab082dc8cc24b32ed38
malini <malinikun2020@gmail.com>
2019-01-13 14:37:23 -0500

    musicsheet

--- malinicode/sheetMusic.java
+++ malinicode/sheetMusic.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.util.Random;
-
-Random rand = new Random();
 
 public class sheetMusic {
 
@@ -24,31 +21,43 @@ public class sheetMusic {
 
   public static int[] pickAScale() {
     int[][] scales;
-
-
-
+    scales  = new int[][] {
+      {0,2,3,5,7,8,10,12},
+      {1,3,4,6,8,9,11,13},
+      {2,4,5,7,9,10,12,14},
+      {3,5,6,8,10,11,13,15}
+    };
   }
   public static double[] verse(int[] scale) {
-      int[] pickScale = ArrayTools.shuffle(scale);
-
+      int[] pickScale = scale;
+//needrandom
       int note1 = pickScale[0];
       int note2 = pickScale[1];
       int note3 = pickScale[2];
+      int note4 = pickScale[3];
+//      int note5 = pickScale[4];
 
-			int duration1 = rand.nextInt(4) + 1;
-			int duration2 = rand.nextInt(4) + 1;
-			int duration3 = rand.nextInt(4) + 1;
+//needrandom
+      int time1 = 3;
+      //int time2 = 3);
+      //int time3 = (20,28);
+    //  int time4 = (20,28);
+    //  int time5 = (20,28);
 
-      double[] a = MusicLibrary.majorChord(note1, duration1);
-      double[] b = MusicLibrary.majorChord(note2, duration2);
-      double[] c = MusicLibrary.majorChord(note3, duration3);
+      double[] a = MusicLibrary.minorChord(note1, 3);
+      double[] b = MusicLibrary.minorChord(note2, 3);
+      double[] c = MusicLibrary.minorChord(note3, 3);
+      double[] d = MusicLibrary.minorChord(note4, 3);
+//      double[] e = MusicLibrary.minorChord(note5, time5);
 
-      System.out.println("\nChorus:");
-      System.out.println(note1+" "+duration1+" Major");
-      System.out.println(note2+" "+duration2+" Major");
-      System.out.println(note3+" "+duration3+" Major");
+      System.out.println("\nVerse:");
+      System.out.println(note1+" "+time1+" Minor");
+      System.out.println(note2+" "+time2+" Minor");
+      System.out.println(note3+" "+time3+" Minor");
+      System.out.println(note4+" "+time4+" Minor");
+  //    System.out.println(note5+" "+time5+" Minor");
 
-      double[] d = ArrayTools.concatenateArray(ArrayTools.concatenateArray(a, b), c);
-      double[] array = ArrayTools.concatenateArray(d,d);
+      double[] concat1 = MusicTools.concatArray(MusicTools.concatArray(a, b), MusicTools.concatArray(c,d));
+      double[] array = ArrayTools.concatArray(concat1,conncat1);
       return array;
   }
