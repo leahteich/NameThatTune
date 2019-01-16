d1df69054c45d6a19163517aaa7465416ce4a98e
malini <malinikun2020@gmail.com>
2019-01-16 09:45:54 -0500

    sheetmusic update

--- malinicode/MusicLibrary.java
+++ malinicode/MusicLibrary.java
@@ -22,126 +22,77 @@
  ******************************************************************************/
 
 public class MusicLibrary {
+  public static double[] harmonic(int pitch, double duration) {
+    double hz = 440.0 * Math.pow(2, pitch / 12.0);
+    double[] a  = pitch(hz, duration);
+    double[] hi = pitch(2*hz, duration);
+    double[] lo = pitch(hz/2, duration);
+    double[] h  = MusicTools.weightedAddArray(hi, lo, 0.5, 0.5);
 
-    // return weighted sum of two arrays
-    public static double[] sum(double[] a, double[] b, double awt, double bwt) {
-
-        // precondition: arrays have the same length
-        assert a.length == b.length;
-
-        // compute the weighted sum
-        double[] c = new double[a.length];
-        for (int i = 0; i < a.length; i++) {
-            c[i] = a[i]*awt + b[i]*bwt;
-        }
-        return c;
-    }
-    public static double[] pitch(double hz, double duration) {
-        int n = (int) (StdAudio.SAMPLE_RATE * duration);
-        double[] a = new double[n+1];
-        for (int i = 0; i <= n; i++) {
-            a[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
-        }
-        return a;
-    } //pitch
-
-//new array with weighted sum
-    // create a pure tone of the given frequency for the given duration
-    //trim should operate on a sin wave array trim is applied on a wave file not the sheet music
-    //save wav version of song
-    public static double[] tone(double hz, double duration) {
-        int n = (int) (StdAudio.SAMPLE_RATE * duration);
-        double[] a = new double[n+1];
-        for (int i = 0; i <= n; i++) {
-            a[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
-        }
-        return a;
-    }
-    // create a note with harmonics of the given pitch and duration
-    // (where 0 = concert A)
-    //tone makes the waves//MusicTools.scale
-    public static double[] note(int pitch, double duration) {
-        double hz = 440.0 * Math.pow(2, pitch / 12.0);
-        double[] a  = tone(hz, duration);
-        double[] hi = tone(2*hz, duration);
-        //adds a high octave
-        double[] lo = tone(hz/2, duration);
-        //adds a low octave
-        double[] h  = sum(hi, lo, 0.5, 0.5);
-        return sum(a, h, 0.5, 0.5);
-    }
-    public static double[] majorChord(int pitch, double duration) {
-        double hz = 440.0 * Math.pow(2, pitch / 12.0);
-        double firstm = 440.0 * Math.pow(2, (pitch + 4) / 12.0);
-        double secondm = 440.0 * Math.pow(2, (pitch + 7) / 12.0);
-        //sets the 3 in major chord
-        double[] a  = pitch(hz, duration);
-        double[] mid = pitch(firstm, duration);
-        double[] hi = pitch(secondm, duration);
-        double[] h  = MusicTools.weightedAddArray(mid, hi, 0.5, 0.5);
-        return MusicTools.weightedAddArray(a, h, 0.5, 0.5);
-    } //majorChord
-    //so major chord, minor chord, using the methods, create a 2-4 min composition
-
-    public static double[] minorChord(int pitch, double duration) {
-        double hz = 440.0 * Math.pow(2, pitch / 12.0);
-        double firstm = 440.0 * Math.pow(2, (pitch + 3) / 12.0);
-        double secondm = 440.0 * Math.pow(2, (pitch + 7) / 12.0);
-//sets the 3 in the minor chord
-        double[] a  = pitch(hz, duration);
-        double[] mid = pitch(firstm, duration);
-        double[] hi = pitch(secondm, duration);
-//using MusicTools, and weightedAddArray, creating minor chord
-        double[] h  = MusicTools.weightedAddArray(mid, hi, 0.5, 0.5);
-        return MusicTools.weightedAddArray(a, h, 0.5, 0.5);
-    } //minorChord
-//creating libraries and playing libraries
-//harmonic adds 3 together, takes it to original pitch and
-//input random numbers, form major chords and minor chords, and change these values in order, repeat them, in order to make a song
-public static double[] fadeIn(double[] array, int in) {
-    int fadeInDuration = (int) (StdAudio.SAMPLE_RATE * in);
-    double[] fadeInArray = new double[array.length+1];
-      for (int i =0; i<fadeInDuration; i++) {
-        fadeInArray[i]= array[i]*i/fadeInDuration;
-      }
-      return fadeInArray;
-}//fade in
-
-public static double[] fadeOut(double[] array, int out) {
-    int fadeOutDuration = (int) (StdAudio.SAMPLE_RATE * out);
-    double[] fadeOutArray = new double[array.length];
-    for (int i =0; i<   fadeOutDuration; i++) {
-      fadeOutArray[fadeOutArray.length-1-i]= array[fadeOutArray.length-1-i]*i/fadeOutDuration;
-//percentage
-    }
-    return fadeOutArray;
-} //fade out
-public static double[] changeVolume(double[] array, double volume) {
-    return MusicTools.scaleArray(array, volume);
-}
-/*public static double[] clip(double[] array){
-  double[] clipArray = new double[array.length];
-  for (int i=0; i <clipArray.length; i++){
-
+    return MusicTools.weightedAddArray(a, h, 0.5, 0.5);
   }
-return clip;
-}*/
-    public static void main(String[] args) {
 
-        // read in pitch-duration pairs from standard input
-        while (!StdIn.isEmpty()) {
-            int pitch = StdIn.readInt();
-            double duration = StdIn.readDouble();
-            double[] a = note(pitch, duration);
-            //double[] b = minorChord(pitch,duration);
-            //StdAudio.play(a);
-            //StdAudio.save("harmonic.wav", (fadeIn(minorChord(pitch,duration),2)));
-            //StdAudio.play(a);
-            //StdAudio.play(minorChord(pitch,duration));
-             StdAudio.play(fadeOut(minorChord(pitch,duration),1));
+  public static double[] minorChord(int pitch, double duration) {
+      double hz = 440.0 * Math.pow(2, pitch / 12.0);
+      double m = 440.0 * Math.pow(2, (pitch + 3) / 12.0);
+      double p = 440.0 * Math.pow(2, (pitch + 7) / 12.0);
+      double[] a  = pitch(hz, duration);
+      double[] mid = pitch(m, duration);
+      double[] hi = pitch(p, duration);
+      double[] h  = MusicTools.weightedAddArray(mid, hi, 0.5, 0.5);
+      return MusicTools.weightedAddArray(a, h, 0.5, 0.5);
+  } //minorChord
 
-           //  StdAudio.play.majorChord(a);
-            ///StdAudio.play()
-        }
+  public static double[] majorChord(int pitch, double duration) {
+      double hz = 440.0 * Math.pow(2, pitch / 12.0);
+      double m = 440.0 * Math.pow(2, (pitch + 4) / 12.0);
+      double p = 440.0 * Math.pow(2, (pitch + 7) / 12.0);
+      double[] a  = pitch(hz, duration);
+      double[] mid = pitch(m, duration);
+      double[] hi = pitch(p, duration);
+      double[] h  = MusicTools.weightedAddArray(mid, hi, 0.5, 0.5);
+      return MusicTools.weightedAddArray(a, h, 0.5, 0.5);
+  } //majorChord
+
+  public static double[] fadeIn(double[] note, double secondsToFade) {
+          double n = (double) StdAudio.SAMPLE_RATE;
+          int length = (int) Math.ceil(n * secondsToFade);
+          double[] finalA = new double[length];
+          for (int i = 0; i < length; i += 1) {
+              finalA[i] = note[i] * ((double) i / (n * secondsToFade));
+          }
+          return finalA;
+      }//fade in
+  public static double[] pitch(double hz, double duration) {
+      int n = (int) (StdAudio.SAMPLE_RATE * duration);
+      double[] a = new double[n+1];
+      for (int i = 0; i <= n; i++) {
+          a[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
+      }
+      return a;
+  } //pitch
+
+  public static double[] sum(double[] a, double[] b, double awt, double bwt) {
+    return MusicTools.weightedAddArray(a, b, awt, bwt);
+  } //sum
+
+  public static double[] trim(double[] array) {
+    int leadzeroes = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i]==0) {
+        leadzeroes++;
+      }
     }
+
+    double[] newArray = new double[array.length-leadzeroes];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = array[leadzeroes];
+    }
+
+
+  public static double makeHZ(int pitch){
+    double hz = 440.0 * Math.pow(2, pitch / 12.0);
+    return hz;
+  }
+
 }
--- malinicode/sheetMusic.java
+++ malinicode/sheetMusic.java
@@ -13,14 +13,14 @@ public class sheetMusic {
   		double[] verse = verse(scale1);
 			double[] bridge = bridge(scale2);
 
-		    double[] a = musicPlan(chorus,verse,bridge);
-		    StdAudio.save("songnumber"+25+".wav", a);
-		    StdAudio.play(a);
-//why does text file not work
+		  double[] a = musicPlan(chorus,verse,bridge);
+		  StdAudio.save("songnumber"+25+".wav", a);
+		  StdAudio.play(a);
 	}
 
   public static int[] chooseMinor() {
     int[][] scales;
+
     scales  = new int[][] {
     {0,2,3,5,7,8,10,12},
     {1,3,4,6,8,9,11,13},
@@ -35,11 +35,13 @@ public class sheetMusic {
     {10,12,13,15,17,18,20,22},
     {11,13,14,16,18,19,21,23}
     };
+
 		return scales[2];
   }
 
   public static int[] chooseMajor() {
     int[][] scales;
+
     scales = new int[][] {
       {3,5,7,8,10,12,14,15},
       {5,7,9,10,12,14,16,17},
@@ -54,8 +56,10 @@ public class sheetMusic {
       {11,13,15,16,18,20,22,23},
       {1,3,5,6,8,10,12,13}
     };
+
     return scales[3];
   }
+
   public static double[] verse(int[] scale1) {
       int[] pickScale = scale1;
 			int note1 = pickScale[StdRandom.uniform(1,8)];
@@ -63,24 +67,18 @@ public class sheetMusic {
       int note3 = pickScale[StdRandom.uniform(1,8)];
       int note4 = pickScale[StdRandom.uniform(1,8)];
 
-//needrandom
       int time1 = StdRandom.uniform(1,3);
 			int time2 = StdRandom.uniform(1,3);
 			int time3 = StdRandom.uniform(1,3);
 			int time4 = StdRandom.uniform(1,3);
 
-
-
       double[] a = MusicLibrary.minorChord(note1, time1);
       double[] b = MusicLibrary.minorChord(note2, time2);
       double[] c = MusicLibrary.minorChord(note3, time3);
       double[] d = MusicLibrary.minorChord(note4, time4);
-//      double[] e = MusicLibrary.minorChord(note5, time5);
 
       System.out.println("\nVerse:");
       System.out.println(note1+" "+" Minor");
-
-  //    System.out.println(note5+" "+time5+" Minor");
 
       double[] concat1 = MusicTools.concatArray(MusicTools.concatArray(a, b), MusicTools.concatArray(c,d));
       double[] array = MusicTools.concatArray(concat1,concat1);
@@ -89,29 +87,16 @@ public class sheetMusic {
 
   public static double[] chorus(int[] scale2) {
       int[] pickScale = scale2;
-  //needrandom
+
       int note1 = pickScale[0];
       int note2 = pickScale[3];
       int note3 = pickScale[6];
       int note4 = pickScale[1];
-  //      int note5 = pickScale[4];
-
-  //needrandom
-    //  int time1 = StdRandom.uniform(1,3);
-			//int time2 = StdRandom.uniform(1,3);
-		//	int time3 = StdRandom.uniform(1,3);
-			//int time4 = StdRandom.uniform(1,3);
 
       double[] a = MusicLibrary.majorChord(note1, 3);
       double[] b = MusicLibrary.majorChord(note2, 3);
       double[] c = MusicLibrary.majorChord(note3, 3);
       double[] d = MusicLibrary.majorChord(note4, 3);
-  //      double[] e = MusicLibrary.minorChord(note5, time5);
-
-      System.out.println("\nVerse:");
-      System.out.println(note1+" "+" Major");
-
-  //    System.out.println(note5+" "+time5+" Minor");
 
       double[] concat2 = MusicTools.concatArray(MusicTools.concatArray(a, b), MusicTools.concatArray(c,d));
       double[] array = MusicTools.concatArray(concat2,concat2);
@@ -120,37 +105,22 @@ public class sheetMusic {
 
   public static double[] bridge(int[] scale2) {
       int[] pickScale = scale2;
-  //needrandom
+
       int note1 = pickScale[1];
       int note2 = pickScale[4];
       int note3 = pickScale[3];
       int note4 = pickScale[2];
-  //      int note5 = pickScale[4];
-
-  //needrandom
-    //  int time1 = 3;
-      //int time2 = 3);
-      //int time3 = (20,28);
-    //  int time4 = (20,28);
-    //  int time5 = (20,28);
 
       double[] a = MusicLibrary.majorChord(note1, 3);
       double[] b = MusicLibrary.majorChord(note2, 3);
       double[] c = MusicLibrary.majorChord(note3, 3);
       double[] d = MusicLibrary.majorChord(note4, 3);
-  //      double[] e = MusicLibrary.minorChord(note5, time5);
-
-    /*  System.out.println("\nVerse:");
-      System.out.println(note1+" "+" Major");
-      System.out.println(note2+" "+" Major");
-      System.out.println(note3+" "+" Major");
-      System.out.println(note4+" "+" Major");
-  //    System.out.println(note5+" "+time5+" Minor");*/
 
       double[] concat3 = MusicTools.concatArray(MusicTools.concatArray(a, b), MusicTools.concatArray(c,d));
       double[] array = MusicTools.concatArray(concat3,concat3);
       return array;
     }
+
     public static double[] musicPlan(double[] chorus, double[] verse, double[] bridge) {
       double[] array = new double[8];
       array = MusicTools.concatArray(array,verse);
