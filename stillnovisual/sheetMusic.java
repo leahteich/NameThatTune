3e2358a3734f7d11116b20eab4afbabe3ef685cc
leah <33074399+leahteich@users.noreply.github.com>
2019-01-23 10:16:57 -0500

    deleting random stuff

--- stillnovisual/sheetMusic.java
+++ stillnovisual/sheetMusic.java
@@ -4,31 +4,31 @@ import java.lang.System;
 
 public class sheetMusic {
 
-        public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 //stackoverflow- https://stackoverflow.com/questions/26769345/convert-a-wav-file-to-a-txt-file
 
-                        int songNum = StdRandom.uniform(0,10000);
+			int songNum = StdRandom.uniform(0,10000);
 
-                        PrintStream out = new PrintStream(new FileOutputStream("song"+songNum+".txt"));
+			PrintStream out = new PrintStream(new FileOutputStream("song"+songNum+".txt"));
 
-                        System.setOut(out);
+			System.setOut(out);
 
-         int[] scale1 = chooseMinor();
+    	int[] scale1 = chooseMinor();
       int[] scale2 = chooseMajor();
 
-                        generateText(scale1, scale2);
+			generateText(scale1, scale2);
 
-         double[] chorus = chorus(scale2);
-                 double[] verse = verse(scale1);
-                        double[] bridge = bridge(scale1);
+    	double[] chorus = chorus(scale2);
+  		double[] verse = verse(scale1);
+			double[] bridge = bridge(scale1);
 
-                  double[] a = playFullSong(chorus, verse, bridge);
+		  double[] a = playFullSong(chorus, verse, bridge);
 
 //https://stackoverflow.com/questions/1994255/how-to-write-console-output-to-a-txt-file
-                   StdAudio.save("song"+songNum+".wav", a);
+		   StdAudio.save("song"+songNum+".wav", a);
 
-                   //StdAudio.play(a);
-                }
+		   //StdAudio.play(a);
+		}
 
   public static int[] chooseMinor() {
     int[][] scales;
@@ -46,7 +46,7 @@ public class sheetMusic {
     {10,12,13,15,17,18,20,22},
     {11,13,14,16,18,19,21,23}
     };
-                return MusicLibrary.shuffle(scales[StdRandom.uniform(0,12)]);
+		return MusicLibrary.shuffle(scales[StdRandom.uniform(0,12)]);
   }
 
   public static int[] chooseMajor() {
@@ -73,94 +73,94 @@ public class sheetMusic {
   public static double[] verse(int[] scale1) {
       int[] pickScale = scale1;
 
-                        int noteNumber = StdRandom.uniform(20,40);
-                        int[] notes = new int[noteNumber];
-                        double[] times = new double[noteNumber];
+			int noteNumber = StdRandom.uniform(20,40);
+			int[] notes = new int[noteNumber];
+			double[] times = new double[noteNumber];
 
-                        double[] concatOrig = {};
+			double[] concatOrig = {};
 
-                        for (int i = 0; i < noteNumber; i++) {
-                                notes[i] = pickScale[StdRandom.uniform(0,4)];
-                                times[i] = StdRandom.uniform(.1,.5);
+			for (int i = 0; i < noteNumber; i++) {
+				notes[i] = pickScale[StdRandom.uniform(0,4)];
+				times[i] = StdRandom.uniform(.1,.5);
 
-                                String currentNote = Integer.toString(notes[i]);
-                                String currentTime = Double.toString(times[i]);
+				String currentNote = Integer.toString(notes[i]);
+				String currentTime = Double.toString(times[i]);
 
-                                System.out.println(currentNote + " " + currentTime);
+				System.out.println(currentNote + " " + currentTime);
 
-                                double[] noteToPlay = MusicLibrary.minorChord(notes[i], times[i]);
+				double[] noteToPlay = MusicLibrary.minorChord(notes[i], times[i]);
 
-                                concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
-                        }
+				concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
+			}
 
-                        concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
+			concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
       return concatOrig;
   }
 
   public static double[] chorus(int[] scale2) {
       int[] pickScale = scale2;
 
-                        int noteNumber = StdRandom.uniform(5,10);
-                        int[] notes = new int[noteNumber];
-                        double[] times = new double[noteNumber];
+			int noteNumber = StdRandom.uniform(5,10);
+			int[] notes = new int[noteNumber];
+			double[] times = new double[noteNumber];
 
-                        double[] concatOrig = {};
+			double[] concatOrig = {};
 
-                        for (int i = 0; i < noteNumber; i++) {
-                                notes[i] = pickScale[StdRandom.uniform(0,4)];
-                                times[i] = StdRandom.uniform(1,2);
+			for (int i = 0; i < noteNumber; i++) {
+				notes[i] = pickScale[StdRandom.uniform(0,4)];
+				times[i] = StdRandom.uniform(1,2);
 
-                                String currentNote = Integer.toString(notes[i]);
-                                String currentTime = Double.toString(times[i]);
+				String currentNote = Integer.toString(notes[i]);
+				String currentTime = Double.toString(times[i]);
 
-                                System.out.println(currentNote + " " + currentTime);
+				System.out.println(currentNote + " " + currentTime);
 
-                                double[] noteToPlay = MusicLibrary.majorChord(notes[i], times[i]);
+				double[] noteToPlay = MusicLibrary.majorChord(notes[i], times[i]);
 
-                                concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
-                        }
+				concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
+			}
 
-                        concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
+			concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
       return concatOrig;
 
   }
 
   public static double[] bridge(int[] scale2) {
       int[] pickScale = scale2;
-                        int[] notes = new int[4];
+			int[] notes = new int[4];
 
       int time1 = StdRandom.uniform(2,4);
 
-                        double[] concatOrig = {};
+			double[] concatOrig = {};
 
-                        for (int i = 0; i<4; i++) {
-                                notes[i] = pickScale[StdRandom.uniform(1,8)];
+			for (int i = 0; i<4; i++) {
+				notes[i] = pickScale[StdRandom.uniform(1,8)];
 
-                                String currentNote = Integer.toString(notes[i]);
-                                String currentTime = Double.toString(time1);
+				String currentNote = Integer.toString(notes[i]);
+				String currentTime = Double.toString(time1);
 
-                                System.out.println(currentNote + " " + currentTime);
+				System.out.println(currentNote + " " + currentTime);
 
-                                double[] noteToPlay = MusicLibrary.harmonic(notes[i], time1);
+				double[] noteToPlay = MusicLibrary.harmonic(notes[i], time1);
 
-                                concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
-                        }
+				concatOrig = MusicTools.concatArray(concatOrig, noteToPlay);
+			}
 
-                        concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
+			concatOrig = MusicTools.concatArray(concatOrig, concatOrig);
       return concatOrig;
     }
 
 
-        public static void generateText(int[] scale1, int[] scale2) {
-                verse(scale1);
-                chorus(scale2);
-                verse(scale1);
-                chorus(scale2);
-                verse(scale1);
-                bridge(scale2);
-                chorus(scale2);
-                chorus(scale2);
-        }
+	public static void generateText(int[] scale1, int[] scale2) {
+		verse(scale1);
+		chorus(scale2);
+		verse(scale1);
+		chorus(scale2);
+		verse(scale1);
+		bridge(scale2);
+		chorus(scale2);
+		chorus(scale2);
+	}
 
    public static double[] playFullSong(double[] chorus, double[] verse, double[] bridge) {
       double[] array = new double[8];
